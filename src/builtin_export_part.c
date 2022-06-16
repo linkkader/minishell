bb2e30eb02261373f0e7c3897e76bd86941e5f67
linkkader <59131542+linkkader@users.noreply.github.com>
2022-06-16 18:10:12 +0100

    norminette export

--- src/builtin_export_part.c
+++ src/builtin_export_part.c
@@ -39,13 +39,13 @@ static void	part_part(char *str, t_util *util)
 		temp = temp->next;
 	}
 	if (t != NULL)
-		value = strjoin(t,
-						ft_substr(str, util->j + 2, ft_strlen(str) - util->j - 1));
+		value = strjoin(t, ft_substr(str, util->j + 2,
+					ft_strlen(str) - util->j - 1));
 	else
 		value = ft_substr(str, util->j + 2, ft_strlen(str) - util->j - 1);
 	free(t);
 	export_value(ft_substr(str, 0, util->j),
-				 value, util->var, util->is_in_export);
+		value, util->var, util->is_in_export);
 }
 
 static void	part(char *str, t_util *util)
@@ -61,13 +61,13 @@ static void	part(char *str, t_util *util)
 			&& str[util->j + 1] == '=')
 			part_part(str, util);
 		if (!(ft_isalpha(str[util->j]) == 1 || ft_isalnum(str[util->j]) == 1
-			  ||str[util->j] == '_'))
+				||str[util->j] == '_'))
 		{
 			util->bool = true;
 			if (str[util->j] == '=')
 				export_value(ft_substr(str, 0, util->j),
-							 ft_substr(str, util->j + 1, ft_strlen(str) - util->j - 1),
-							 util->var, util->is_in_export);
+					ft_substr(str, util->j + 1, ft_strlen(str) - util->j - 1),
+					util->var, util->is_in_export);
 			else
 				not_an_identifier(util->var);
 			break ;
@@ -78,7 +78,7 @@ static void	part(char *str, t_util *util)
 
 //[a-zA-Z_][a-zA-Z0-9_]
 void	try_export_value(char **sp, t_var *var,
-						 t_bool is_in_export, int start)
+				t_bool is_in_export, int start)
 {
 	t_util	util;
 
@@ -96,7 +96,7 @@ void	try_export_value(char **sp, t_var *var,
 		if (util.is_in_export == true && util.j == ft_strlen(sp[util.i - 1]))
 		{
 			export_value(ft_substr(sp[util.i - 1], 0, util.j),
-						 NULL, var, util.is_in_export);
+				NULL, var, util.is_in_export);
 			continue ;
 		}
 		ft_putstr_fd(ERR_CMD, var->console_fd);
